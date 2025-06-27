1adef7de7b6054c01af484d004c5646a75edac30
assignment-sets <mondalgourab140@gmail.com>
2025-06-27 20:32:06 +0530

    week 2 complete

--- week_2/week_2_PLSQL_programming/Exercise_1_Control_Structures/code/src.sql
+++ week_2/week_2_PLSQL_programming/Exercise_1_Control_Structures/code/src.sql
@@ -1,1 +1,82 @@
-hi
+-- Schema
+
+CREATE TABLE customers (
+    cust_id NUMBER PRIMARY KEY,
+    cust_name VARCHAR2(50) NOT NULL,
+    age NUMBER(3) NOT NULL,
+    vip NUMBER(1) DEFAULT 0 NOT NULL,
+    balance NUMBER(12,2)
+);
+
+CREATE TABLE loans (
+    loan_id NUMBER PRIMARY KEY,
+    cust_id NUMBER NOT NULL,
+    amt NUMBER(10,2) NOT NULL,
+    rate NUMBER(4,2) NOT NULL,
+    due_date DATE NOT NULL,
+    CONSTRAINT fk_cust_id FOREIGN KEY (cust_id) REFERENCES customers(cust_id)
+);
+
+INSERT ALL
+    INTO customers (cust_id, cust_name, age, vip, balance) VALUES (1, 'John Smith', 35, 1, 7500)
+    INTO customers (cust_id, cust_name, age, vip, balance) VALUES (2, 'Emily Davis', 65, 0, 12000)
+    INTO customers (cust_id, cust_name, age, vip, balance) VALUES (3, 'Michael Johnson', 45, 1, 12000)
+    INTO customers (cust_id, cust_name, age, vip, balance) VALUES (4, 'Sophia Lee', 70, 0, 7500)
+    INTO customers (cust_id, cust_name, age, vip, balance) VALUES (5, 'David Brown', 50, 1, 12000)
+SELECT * FROM dual;
+
+INSERT ALL
+  INTO loans (loan_id, cust_id, amt, rate, due_date) VALUES (101, 1, 10000.00, 5.5, TO_DATE('2025-12-01', 'YYYY-MM-DD'))
+  INTO loans (loan_id, cust_id, amt, rate, due_date) VALUES (102, 2, 5000.00, 6.2, TO_DATE('2025-10-15', 'YYYY-MM-DD'))
+  INTO loans (loan_id, cust_id, amt, rate, due_date) VALUES (103, 3, 20000.00, 4.8, TO_DATE('2025-11-20', 'YYYY-MM-DD'))
+  INTO loans (loan_id, cust_id, amt, rate, due_date) VALUES (104, 4, 3000.00, 7.0, TO_DATE('2025-07-05', 'YYYY-MM-DD'))
+  INTO loans (loan_id, cust_id, amt, rate, due_date) VALUES (105, 5, 15000.00, 5.0, TO_DATE('2026-01-10', 'YYYY-MM-DD'))
+SELECT * FROM dual;
+
+
+---------------------------------------------------------------------
+
+
+-- Scenario 1
+BEGIN
+  FOR c IN (SELECT cust_id FROM customers WHERE age > 60) LOOP
+    UPDATE loans
+    SET rate = rate - 1
+    WHERE cust_id = c.cust_id;
+  END LOOP;
+  
+  COMMIT;
+END;
+/
+
+-- Scenario 2
+BEGIN
+  FOR c IN (
+    SELECT cust_id
+    FROM customers
+    where balance > 10000
+  ) LOOP
+    UPDATE customers
+    SET vip = 1
+    WHERE cust_id = c.cust_id;
+  END LOOP;
+  
+  COMMIT;
+END;
+/
+
+-- Scenario 3
+BEGIN
+  FOR l IN (
+    SELECT l.loan_id, l.due_date, c.cust_name
+    FROM loans l
+    JOIN customers c ON l.cust_id = c.cust_id
+    WHERE l.due_date BETWEEN SYSDATE AND SYSDATE + 30
+  ) LOOP
+    DBMS_OUTPUT.PUT_LINE(
+      'Reminder: Loan ' || l.loan_id || ' for customer ' || l.cust_name || 
+      ' is due on ' || TO_CHAR(l.due_date, 'YYYY-MM-DD')
+    );
+  END LOOP;
+END;
+/
--- week_2/week_2_PLSQL_programming/Exercise_3_Stored_Procedures/code/src.sql
+++ week_2/week_2_PLSQL_programming/Exercise_3_Stored_Procedures/code/src.sql
@@ -1,1 +1,113 @@
-hi
+CREATE TABLE savings_accounts (
+    acc_id NUMBER PRIMARY KEY,
+    acc_holder VARCHAR2(50) NOT NULL,
+    balance NUMBER(12,2) NOT NULL,
+    acc_type VARCHAR2(20) DEFAULT 'SAVINGS' NOT NULL
+);
+
+
+INSERT ALL
+  INTO savings_accounts (acc_id, acc_holder, balance) VALUES (1, 'Alice', 5000)
+  INTO savings_accounts (acc_id, acc_holder, balance) VALUES (2, 'Bob', 10000)
+  INTO savings_accounts (acc_id, acc_holder, balance) VALUES (3, 'Charlie', 7500)
+  INTO savings_accounts (acc_id, acc_holder, balance) VALUES (4, 'Diana', 12000)
+SELECT * FROM dual;
+
+
+CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest(
+    p_account_type IN VARCHAR2
+)
+AS
+BEGIN
+    UPDATE savings_accounts
+    SET balance = balance + (balance * 0.01)
+    WHERE acc_type = p_account_type;
+
+    COMMIT;
+END;
+/
+
+
+
+BEGIN
+  ProcessMonthlyInterest('SAVINGS');
+END;
+/
+
+
+SELECT * FROM savings_accounts;
+
+----------------------------------------------------------
+
+
+CREATE TABLE employees (
+    emp_id NUMBER PRIMARY KEY,
+    emp_name VARCHAR2(50) NOT NULL,
+    department VARCHAR2(30) NOT NULL,
+    salary NUMBER(10,2) NOT NULL
+);
+
+
+INSERT ALL
+  INTO employees (emp_id, emp_name, department, salary) VALUES (1, 'Malice', 'SALES', 50000)
+  INTO employees (emp_id, emp_name, department, salary) VALUES (2, 'Bob', 'IT', 60000)
+  INTO employees (emp_id, emp_name, department, salary) VALUES (3, 'Charlie', 'SALES', 55000)
+  INTO employees (emp_id, emp_name, department, salary) VALUES (4, 'Diana', 'HR', 52000)
+SELECT * FROM dual;
+
+
+
+create or replace PROCEDURE UpdateEmployeeBonus(
+    p_department in VARCHAR2,
+    p_bonus_percentage in number
+) as
+BEGIN
+    UPDATE employees set salary = salary + (salary*p_bonus_percentage/100)
+    where department = p_department;
+
+    COMMIT;
+END;
+/
+
+
+BEGIN
+    UpdateEmployeeBonus('SALES', 7.5);
+END;
+/
+
+SELECT * FROM employees;
+
+
+------------------------------------------------------
+
+
+create or replace PROCEDURE TransferFunds(
+    p_sender_acc_id in NUMBER,
+    p_rec_acc_id in NUMBER,
+    p_transfer_amt in number
+) as
+    v_balance number;
+BEGIN
+    select balance INTO v_balance
+    from savings_accounts
+    where acc_id = p_sender_acc_id
+    for UPDATE;
+
+    if v_balance < p_transfer_amt then
+        RAISE_APPLICATION_ERROR(-20001, 'Insufficient funds');
+    else
+        update savings_accounts set balance = balance - p_transfer_amt
+        where acc_id = p_sender_acc_id;
+
+        update savings_accounts set balance = balance + p_transfer_amt
+        where acc_id = p_rec_acc_id;
+        
+        COMMIT;
+    end if;
+END;
+/
+
+BEGIN
+  TransferFunds(1, 2, 1000);
+END;
+/
